9698f8064c11698cf54d5c458de0c9c7afdfd3f3
Skopin_mi@mail.ru <Skopin1981>
2020-07-28 21:49:20 +0300

    hw2

--- src/main/java/ReadFilePages.java
+++ src/main/java/ReadFilePages.java
@@ -1,28 +1,33 @@
-import java.io.FileNotFoundException;
+
+import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.util.ArrayList;
+
 
 public class ReadFilePages {
 
     private static int index;
     private static final int countCharsPerPages = 1800;
     private static boolean hasNextPage = true;
+    private static String file = "src\\text";
     public static void main(String[] args) {
+        System.out.println("Начало программы. Размер файла " + new File(file).length() + " байт.");
         long t = System.currentTimeMillis();
         index = 0;
         try {
-            RandomAccessFile randomAccessFile = new RandomAccessFile("src\\text", "r");
+            RandomAccessFile randomAccessFile = new RandomAccessFile(file, "r");
             int countOfPages = 1;
             while (hasNextPage) {
+                long timeOnePage = System.currentTimeMillis();
                 printPage(getOnePage(randomAccessFile, countCharsPerPages));
                 System.out.println("Page №" + countOfPages);
+                System.out.println("reading time " + (System.currentTimeMillis() - timeOnePage));
                 countOfPages++;
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        System.out.println(System.currentTimeMillis() - t);
+        System.out.println("Время выполнения программы " + (System.currentTimeMillis() - t));
     }
     public static int [] getOnePage (RandomAccessFile randomAccessFile, int countCharsPerPages) {
         int[] result = new int[countCharsPerPages];
