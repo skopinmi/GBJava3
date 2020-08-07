22e0a1042bb033eb9a0ce73a353f1aaa27c70906
Skopin_mi@mail.ru <Skopin1981>
2020-08-07 13:46:08 +0300

    HW Lesson6

--- src/test/java/TestArrayFromArray.java
+++ src/test/java/TestArrayFromArray.java
@@ -8,14 +8,14 @@ public class TestArrayFromArray {
 //    start from 4
     @Test
     public void test1 () {
-        int [] a = {4, 2, 3, 4, 5};
-        int [] b = {2, 3, 4, 5};
+        int [] a = {4, 2, 3, 1, 5};
+        int [] b = {2, 3, 1, 5};
         Assert.assertArrayEquals(b, arrayFromArray.arrayAfterFour(a));
     }
 //    4 end
     @Test
     public void test2 () {
-        int [] a = {1, 2, 3, 4, 4};
+        int [] a = {1, 2, 3, 5, 4};
         int [] b = {};
         Assert.assertArrayEquals(b, arrayFromArray.arrayAfterFour(a));
     }
@@ -23,7 +23,7 @@ public class TestArrayFromArray {
     @Test
     public void test3 () {
         int [] a = {1, 2, 4, 4, 5};
-        int [] b = {2, 3, 4, 5};
+        int [] b = { 5};
         Assert.assertArrayEquals(b, arrayFromArray.arrayAfterFour(a));
     }
 //    4 more 2
--- src/test/java/TestMassArrayFromArray.java
+++ src/test/java/TestMassArrayFromArray.java
@@ -1,2 +1,42 @@
+import lesson6.ArrayFromArray;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.Arrays;
+import java.util.Collection;
+@RunWith(Parameterized.class)
 public class TestMassArrayFromArray {
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new int[]{1,1,1,1}, false},
+                {new int[]{4,4,4,4}, false},
+                {new int[]{1,4,1,4}, true},
+                {new int[]{1,4,6,1}, false},
+                {new int[]{0,0,0,0}, false}
+        });
+    }
+    private int[] a;
+    private boolean b;
+
+    public TestMassArrayFromArray (int[] a, boolean b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    ArrayFromArray arrayFromArray;
+
+    @Before
+    public void init () {
+        arrayFromArray = new ArrayFromArray();
+    }
+
+    @Test
+    public void test () {
+        Assert.assertEquals(b, arrayFromArray.hasOneAndFour(a));
+    }
+
 }
