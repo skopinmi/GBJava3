4b3dbe81cc55aadb25c801b907ae19477758fcc7
Skopin <Skopin_MI@mail.ru>
2020-08-12 12:26:33 +0300

    2. Написать программу для проверки ДЗ
    Exception in thread "main" java.lang.NoClassDefFoundError: lesson1/lesson1 (wrong name: Lesson1)
    	at java.base/java.lang.ClassLoader.defineClass1(Native Method)
    	at java.base/java.lang.ClassLoader.defineClass(ClassLoader.java:1017)
    	at java.base/java.security.SecureClassLoader.defineClass(SecureClassLoader.java:151)
    	at java.base/java.net.URLClassLoader.defineClass(URLClassLoader.java:514)
    	at java.base/java.net.URLClassLoader$1.run(URLClassLoader.java:422)
    	at java.base/java.net.URLClassLoader$1.run(URLClassLoader.java:416)
    	at java.base/java.security.AccessController.doPrivileged(AccessController.java:691)
    	at java.base/java.net.URLClassLoader.findClass(URLClassLoader.java:415)
    	at java.base/java.lang.ClassLoader.loadClass(ClassLoader.java:589)
    	at java.base/java.net.FactoryURLClassLoader.loadClass(URLClassLoader.java:855)
    	at java.base/java.lang.ClassLoader.loadClass(ClassLoader.java:522)
    	at lesson7.DZ.TestDZ.main(TestDZ.java:20)

--- src/main/java/lesson7/DZ/TestDZ.java
+++ src/main/java/lesson7/DZ/TestDZ.java
@@ -1,8 +1,11 @@
 package lesson7.DZ;
 
 import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.HashMap;
@@ -10,29 +13,63 @@ import java.util.HashMap;
 public class TestDZ {
 
     public static void main(String[] args) {
-        String path = "src/main/java/lesson7/DZ/lesson1";
-        File folder = new File(path);
-        File[] files = folder.listFiles();
-        HashMap<File, Method[]> dz = new HashMap<>();
-        for (File a : files) {
-            System.out.println(a.getName());
-            dz.put(a, getMethodsFromFile(a, folder));
-        }
-
-
-    }
-    public static Method[] getMethodsFromFile (File file, File folder) {
-        Method[] methods = null;
-        String[] tok = file.getName().split("\\.");
         Class ch = null;
+        Constructor constructor = null;
         try {
-            System.out.println(folder.toURL());
-            ch = URLClassLoader.newInstance(new URL[]{folder.toURL()}).loadClass(tok[0]);
+            URL[] url = new URL[]{new File("D:/lesson1").toURL()};
+            ch = URLClassLoader.newInstance(url).loadClass("lesson1");
         } catch (ClassNotFoundException | MalformedURLException e) {
             e.printStackTrace();
         }
-        methods = ch.getMethods();
-        System.out.println("g");
-        return methods;
+        try {
+            constructor = ch.getConstructor();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        Object l = constructor;
+
+        Method[] methods = ch.getDeclaredMethods();
+        for (Method a: methods) {
+            if (!a.getReturnType().equals("void")) {
+                try {
+                    a.invoke(l);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
     }
+/*
+      создает HashMap<File, Method[]> имя файла и маасив всех его методов...
+ */
+//    public static HashMap<File, Method[]> makeMap (File folder) {
+//        String path = "src/main/java/lesson7/DZ/lesson1";
+//        File folder = new File(path);
+//        File[] files = folder.listFiles();
+//        HashMap<File, Method[]> dz = new HashMap<>();
+//        for (File a : files) {
+//            System.out.println(a.getName());
+//            dz.put(a, getMethodsFromFile(a, folder));
+//        }
+//        return null;
+//    }
+
+//    public static Method[] getMethodsFromFile (File file, File folder) {
+//        Method[] methods = null;
+//        String[] tok = file.getName().split("\\.");
+//        Class ch = null;
+//        try {
+//            System.out.println(folder.toURL());
+//            ch = URLClassLoader.newInstance(new URL[]{folder.toURL()}).loadClass(tok[0]);
+//        } catch (ClassNotFoundException | MalformedURLException e) {
+//            e.printStackTrace();
+//        }
+//        methods = ch.getMethods();
+//        System.out.println("g");
+//        return methods;
+//    }
 }
